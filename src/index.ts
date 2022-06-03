ec7d32106ce2f1708b253243d030061a5f7c5a64
wilyyy <walvarez1@my.bcit.ca>
2022-06-03 02:28:01 -0700

    figure out how2add multiple pages then map npc element from data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "./styles/index.css";
 
 //test code
-
 // const redButton = document.querySelector<HTMLButtonElement>(".redbutton");
 // const heading = document.querySelector<HTMLHeadingElement>(".heading");
 
@@ -11,13 +10,50 @@ import "./styles/index.css";
 //    heading.classList.toggle("hidden");
 // });
 
-//fetching npcs test
-const GetNpcs = async (): Promise<any> => {
+// NPC functions--------------------------------
+//fetch NPC data
+const FetchNPCs = async (): Promise<any> => {
    const response = await fetch("https://eldenring.fanapis.com/api/npcs", {
       method: "GET",
    });
-   console.log(response.json());
-   return response.json();
+   const allNPCs = await response.json();
+
+   return allNPCs;
 };
 
-GetNpcs();
+const npcCont = document.querySelector(".npcCont");
+
+//callback function when dom is loaded, to grab .data from response json object
+window.addEventListener("DOMContentLoaded", async () => {
+   //code to grab data from fetch function above
+   let allNPCs = [];
+
+   try {
+      allNPCs = await FetchNPCs();
+   } catch (e) {
+      console.log("Error!");
+      console.log(e);
+   }
+
+   console.log(allNPCs.data);
+
+   //interface for data's object types
+   interface DataValues {
+      id?: string;
+      image?: string;
+      location?: string;
+      name?: string;
+      quote?: string;
+      role?: string;
+   }
+
+   const npcInnerHtml = allNPCs.data.map((el: DataValues, index: number) => {
+      return ``;
+   });
+});
+
+//figure out how to add multiple pages
+// https://stackoverflow.com/questions/60021318/what-is-the-right-way-to-configure-multiple-pages-in-webpack
+
+// map html element with cat into npcinnerhtml variable
+// https://www.youtube.com/watch?v=llnoLJy9fFw&t=332s
